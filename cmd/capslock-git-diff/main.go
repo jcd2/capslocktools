7fc42d230360cd688fc1e1dd0dc0a912ea943b2f
jcd <jcd@golang.org>
2024-09-10 15:00:18 +0000

    capslock-git-diff: more opinionated defaults
    Use intermediate granularity by default, and ignore the UNANALYZED,
    REFLECT and UNSAFE_POINTER capabilities by default.

--- cmd/capslock-git-diff/main.go
+++ cmd/capslock-git-diff/main.go
@@ -47,8 +47,8 @@ import (
 
 var (
 	verbose          = flag.Bool("v", false, "enable verbose logging")
-	granularity      = flag.String("granularity", "", "the granularity to use for comparisons")
-	flagCapabilities = flag.String("capabilities", "", "if non-empty, a comma-separated list of capabilities to pass to capslock")
+	granularity      = flag.String("granularity", "intermediate", "the granularity to use for comparisons")
+	flagCapabilities = flag.String("capabilities", "-UNANALYZED,-REFLECT,-UNSAFE_POINTER", "if non-empty, a comma-separated list of capabilities to pass to capslock")
 )
 
 func vlog(format string, a ...any) {
